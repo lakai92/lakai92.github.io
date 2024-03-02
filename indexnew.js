3b6dc833e6a4c44f883db542e545d8911b2c837f
lakai92 <57639913+lakai92@users.noreply.github.com>
2024-03-02 02:56:03 +0200

    Update indexnew.js

--- indexnew.js
+++ indexnew.js
@@ -1,1 +1,4 @@
-document.querySelector('.selector-button__bg').style.backgroundColor = 'black';
+const element32 = document.querySelector('.selector-button__bg');
+if (element32) {
+    element32.style.backgroundColor = 'black';
+}
