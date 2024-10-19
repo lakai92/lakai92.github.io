3ebf877b044137b402fc4ddf4c1cf62f32bb336b
lakai92 <57639913+lakai92@users.noreply.github.com>
2024-10-19 03:32:21 +0300

    Update indexnew.js

--- indexnew.js
+++ indexnew.js
@@ -2,3 +2,13 @@ const element32 = document.querySelector('.selector-button__bg');
 if (element32) {
     element32.style.backgroundColor = 'black';
 }
+
+// Находим элемент по классу
+const banReasonDescription = document.querySelector('.video-chat-area .user-ban-overlay .user-ban__reason-description');
+
+// Если элемент найден, присваиваем ему display: none
+if (banReasonDescription) {
+    banReasonDescription.style.display = 'none';
+} else {
+    console.log('Элемент не найден');
+}
